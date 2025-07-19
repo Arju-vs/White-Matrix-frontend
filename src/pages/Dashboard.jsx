e84b7513e53f1edacd7f2a4324ead5bb70463fa6
arju-vs <arjunsaseendran2@gmail.com>
2025-07-19 10:33:28 +0530

    minor fixes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center pt-24 md:pt-0'>
-      <div className="flex flex-col md:flex-row justify-center w-full md:w-[85%] h-auto md:h-[640px] mt-2 md:mt-21 ms-0 md:ms-50 gap-2 px-2 md:px-0">
+      <div className="flex flex-col md:flex-row justify-center w-full h-auto md:h-[640px] mt-2 lg:mt-20  ms-0 md:ms-50 gap-2 px-2 md:px-0">
         <div className="border w-full md:w-2/3 rounded-2xl bg-blue-300">
           <DashMain />
         </div>
